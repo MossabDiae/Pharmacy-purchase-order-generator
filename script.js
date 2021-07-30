bbe7bb2e722704684cf6212b755ecd028e6ce285
mossaybo <mossaybo@gmail.com>
2021-07-30 18:51:58 +0100

    added selection based on data keys

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// tablse html will hold generated rows for each mdts group
+// this will avoid regenerating them each time
+let tables_html = []
+
+function setupDom(){
+    // adding select options for groups
+    for (let group in data){
+        let opt = document.createElement("option")
+        opt.innerHTML = group
+        document.querySelector("#grselect").appendChild(opt)
+    }
+}
+
+function RenderTable(select){
+    // Clean table , check if new value is already generated
+    // if not generate it , finaly display it
+    // console.log(select.value)
+    if (tables_html[select.value]) {
+        
+    }
+
+}
+
 function makeRow(rowdict){
     // take a dictionary and return a table row
     let row  = document.createElement("tr")
@@ -11,8 +34,10 @@ function makeRow(rowdict){
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    let table = document.querySelector("#mdtable")
+    setupDom();
+    let table_body = document.createElement("tbody")
     data.dentaire.forEach(element => {
-        table.appendChild(makeRow(element))
+        table_body.appendChild(makeRow(element))
     });
+    document.querySelector("#mdtable").appendChild(table_body)
 })

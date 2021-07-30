503ef08418304f7a071551443f80cb1f98802a49
mossaybo <mossaybo@gmail.com>
2021-07-30 15:35:09 +0100

    initial com, linked stuff and ready to start

---
+++ script.js
@@ -0,0 +1,15 @@
+// <div id="zone" style="display: none;"></div>
+
+// function printZone() 
+// {
+// 	var zi = document.getElementById('zone').innerHTML;
+// 	var f = window.open("", "ZoneImpr", "height=500, width=600,toolbar=0, menubar=0, scrollbars=1, resizable=1,status=0, location=0, left=10, top=10");
+// 	f.document.body.style.color = '#000000';
+// 	f.document.body.style.backgroundColor = '#FFFFFF';
+// 	f.document.body.style.padding = "10px";
+// 	f.document.title = 'BAC 2021';
+// 	f.document.body.innerHTML += "	" + zi + "	";
+// 	f.window.print();
+ 
+// 	return true;
+// }

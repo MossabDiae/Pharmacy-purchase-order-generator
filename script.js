decc5bd91327c4f0afa771cef4b43539c547fc60
mossaybo <mossaybo@gmail.com>
2021-07-30 19:57:36 +0100

    dynamic table change implemented

--- script.js
+++ script.js
@@ -2,29 +2,40 @@
 // this will avoid regenerating them each time
 let tables_html = []
 
-function setupDom(){
+function setupDom() {
     // adding select options for groups
-    for (let group in data){
+    for (let group in data) {
         let opt = document.createElement("option")
         opt.innerHTML = group
         document.querySelector("#grselect").appendChild(opt)
     }
 }
 
-function RenderTable(select){
-    // Clean table , check if new value is already generated
-    // if not generate it , finaly display it
+function RenderTable(select) {
+    // Check if new value is already generated
+    // if not generate it , finaly replace it
     // console.log(select.value)
-    if (tables_html[select.value]) {
-        
+    let grp_name = select.value
+    old_tbody = document.querySelector("#mdtable tbody")
+    if (tables_html[grp_name]) {
+        // if group already generated and found just show it
+        old_tbody.parentNode.replaceChild(tables_html[grp_name], old_tbody);
+    } else {
+        // value not found, generate it ,store it , display it
+        let table_body = document.createElement("tbody")
+        data[grp_name].forEach(element => {
+            table_body.appendChild(makeRow(element))
+        });
+        tables_html[grp_name] = table_body;
+        old_tbody.parentNode.replaceChild(table_body, old_tbody);
     }
 
 }
 
-function makeRow(rowdict){
+function makeRow(rowdict) {
     // take a dictionary and return a table row
-    let row  = document.createElement("tr")
-    for (let key in rowdict){
+    let row = document.createElement("tr")
+    for (let key in rowdict) {
         let cell = document.createElement("td")
         cell.innerHTML = rowdict[key]
         row.appendChild(cell)
@@ -33,11 +44,6 @@ function makeRow(rowdict){
 }
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     setupDom();
-    let table_body = document.createElement("tbody")
-    data.dentaire.forEach(element => {
-        table_body.appendChild(makeRow(element))
-    });
-    document.querySelector("#mdtable").appendChild(table_body)
 })
